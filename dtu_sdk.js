cd1da605141bf24c1b00f9cede693c293d3a4a1e
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2023-05-10 16:12:04 +0200

    fix for no-cors

--- dtu_sdk.js
+++ dtu_sdk.js
@@ -26,7 +26,7 @@ async function DTU_RX_API_submint_report(report) { // https://developer.mozilla.
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(report), // body data type must match "Content-Type" header
   });
-  return response.json(); // parses JSON response into native JavaScript objects
+  //return response.json(); // parses JSON response into native JavaScript objects // causes u_sdk.js?v=10:29 Uncaught (in promise) SyntaxError: Unexpected end of input (at d.. due to no-cors
 }
 
 /*
