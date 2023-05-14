ce21ab8b4ba55510b1e618db0a4d6f5e80382f74
Alexey <12871810+alexeyhimself@users.noreply.github.com>
2023-05-14 14:18:34 +0200

    idk

--- dtu_sdk.js
+++ dtu_sdk.js
@@ -31,7 +31,7 @@ async function DTU_RX_API_submint_report(report, api_url) { // https://developer
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(report), // body data type must match "Content-Type" header
   });
-  //return response.json(); // parses JSON response into native JavaScript objects // causes u_sdk.js?v=10:29 Uncaught (in promise) SyntaxError: Unexpected end of input (at d.. due to no-cors
+  //return response.json(); // parses JSON response into native JavaScript objects // causes u_sdk.js?v=11:29 Uncaught (in promise) SyntaxError: Unexpected end of input (at d.. due to no-cors
 }
 
 const DEFAULT_CTAG = 'DEMO MVP';
